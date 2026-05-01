c43c04cad9c90192b3dc4a15d5f066582dadab06
Arif <arifmarahman19@gmail.com>
2026-05-01 19:18:33 +0100

    Database service file added

---
+++ src/appwrite/database.js
@@ -0,0 +1,105 @@
+import conf from "../conf/conf";
+import { Client , ID , Databases , Query } from "appwrite";
+
+export class DatabaseService{
+    client = new Client();
+    databases;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+
+        this.databases = new Databases(this.client)
+    }
+    async createPost({title,slug,content,featuredImage,stauts,userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    stauts,
+                    userId
+
+                }
+            )
+            
+        } catch (error) {
+            throw error
+            
+        }
+    }
+     async updatePost(slug,{title,content,featuredImage,stauts,userId}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    stauts,
+                    userId
+
+                }
+            )
+            
+        } catch (error) {
+            throw error
+            
+        }
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true
+            
+        } catch (error) {
+            throw error
+            return false
+            
+        }
+    }
+    async getPost(slug){
+        try {
+            await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        
+            
+        } catch (error) {
+            throw error
+            
+        }
+    }
+    async getPosts(queries = [Query.equal("status" , "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            throw error
+            
+        }
+    }
+
+
+
+}
+
+
+const databaseService = new DatabaseService();
+export default databaseService
