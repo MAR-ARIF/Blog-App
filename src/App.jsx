a2066fcc902d41de96b4e6e20a43ce6857d4d9ba
Arif <arifmarahman19@gmail.com>
2026-05-01 17:54:27 +0100

    Unnecessary css removed and tailwind css configured

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  
 
   return (
     <>
+      <h1 className='text-blue-600'>Blog App</h1>
       
     </>
   )
