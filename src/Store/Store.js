e4791e68663d1ae55a0fcc37b120ad51fcc1c728
Arif <arifmarahman19@gmail.com>
2026-05-03 20:45:10 +0100

    Fixed bug

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "../Slices/authSlice"
+
 
 const store = configureStore({
-    reducer : {
-        
+    reducer: {
+        auth: authSlice,
+
     }
 })
 export default store
