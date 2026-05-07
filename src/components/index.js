23a2b0d17181fc62662762f25604bf6b70878015
Arif <arifmarahman19@gmail.com>
2026-05-07 17:33:04 +0100

    Index.js file modified with new imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,13 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Button from "./Button";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
 
 export {
     Header,
-    Footer
+    Footer,
+    Logo,
+    LogoutBtn,
+    Button
 }
