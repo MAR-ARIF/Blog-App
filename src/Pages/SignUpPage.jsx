33ba083f7610c9f618fd1b0b4e08bff816ba3132
Arif <arifmarahman19@gmail.com>
2026-05-10 12:07:59 +0100

    SignUpPage added

---
+++ src/Pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import SignUp from "../components/SignUp";
+
+function SignUpPage(){
+    return(
+        <div className="py-8">
+            <SignUp />
+
+        </div>
+    )
+}
+export default SignUpPage
