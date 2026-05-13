b4250fdeef912cc3987d8e877f9aa6c405dce813
Arif <arifmarahman19@gmail.com>
2026-05-13 18:38:04 +0100

    Fixed postcard for mobile devices

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,7 +4,7 @@ import storageService from "../appwrite/storage";
 function PostCard({$id , featuredImage, title,userName}){
     return(
         <Link to={`/post/${$id}`}>
-            <div className="bg-gray-100 p-4 rounded-xl w-sm md:w-full md:h-full transition-transform duration-300 hover:scale-105">
+            <div className="bg-gray-100 p-4 rounded-xl w-xs md:w-full md:h-full transition-transform duration-300 hover:scale-105">
                 <div className="w-full flex justify-center mb-4">
                     {featuredImage && (
                         <img
